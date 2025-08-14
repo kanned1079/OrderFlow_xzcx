533f053c99cd38fb884ed7513cb91daace3768b9
kanned1079 <kanned1079@gmail.com>
2025-08-14 17:43:52 +0800

    update rm images

--- app-server/internal/routers/admin_routes.go
+++ app-server/internal/routers/admin_routes.go
@@ -2,6 +2,9 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"path/filepath"
 	"stay-server/internal/middlewares"
 	"stay-server/internal/services/admin"
 )
@@ -20,4 +23,27 @@ func (this *GatewayApp) RegisterAdminRoutes(v1 *gin.RouterGroup) {
 	adminGrp.POST("traders")
 
 	adminGrp.GET("/statistic", adminService.FetchAdminDashboardStatistic)
+
+	adminGrp.GET("/image/clear", func(ctx *gin.Context) {
+		path := "./uploads"
+
+		// 读取目录下的所有文件/文件夹
+		files, err := os.ReadDir(path)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "读取目录失败", "error": err.Error()})
+			return
+		}
+
+		// 遍历并删除
+		for _, file := range files {
+			filePath := filepath.Join(path, file.Name())
+			err = os.RemoveAll(filePath) // RemoveAll 可删除文件或文件夹
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"message": "删除失败", "file": filePath, "error": err.Error()})
+				return
+			}
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "清理完成"})
+	})
 }
